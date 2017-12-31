8fbed088ebeb79f0b434f6e7e633fbd728992b14
Xavier Nara <32371616+xaviernara@users.noreply.github.com>
2017-12-30 21:51:21 -0800

    My first python code

---
+++ Python Examples/Python EX1.py
@@ -0,0 +1,39 @@
+Python 3.6.4 (v3.6.4:d48eceb, Dec 19 2017, 06:54:40) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> print("hello world");
+hello world
+>>> x=8; y=9;
+>>> z=x+y;
+>>> print(z);
+17
+>>> 5+7
+12
+>>> x+8
+16
+>>> (9+5)/3
+4.666666666666667
+>>> 12//4
+3
+>>> 777
+777
+>>> 8+7
+15
+>>> x1= "hello world";
+>>> print(x)
+8
+>>> print(x1)
+hello world
+>>> "She was dead"
+'She was dead'
+>>> "goodbye" + x1
+'goodbyehello world'
+>>> x1 +3
+Traceback (most recent call last):
+  File "<pyshell#16>", line 1, in <module>
+    x1 +3
+TypeError: must be str, not int
+>>> x1*3
+'hello worldhello worldhello world'
+>>> x1 * 5
+'hello worldhello worldhello worldhello worldhello world'
+>>> 
